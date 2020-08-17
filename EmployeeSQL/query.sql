b1a75ca1c71490ac394db18a8525fb771d1c74a9
MichelleH <voirdire96@gmail.com>
2020-08-16 21:33:09 -0700

    Created Sales employee query

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -79,4 +79,17 @@ d.dept_no=de.dept_no;
 
 SELECT first_name, last_name, sex
 FROM employees
-WHERE first_name LIKE 'Hercules' AND last_name LIKE 'B%'
+WHERE first_name LIKE 'Hercules' AND last_name LIKE 'B%';
+
+-- List all employees in the Sales department, 
+-- including their employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+--Add the department employees to the info from the employee table
+INNER JOIN dept_emp de ON
+e.emp_no=de.emp_no
+--Add the departments to the info from the employee table
+INNER JOIN departments d ON
+d.dept_no=de.dept_no
+WHERE d.dept_name LIKE 'Sales';
