cca8a6fe05c31fc85128814e444a938224aab627
MichelleH <voirdire96@gmail.com>
2020-08-22 16:35:37 -0700

    Changed column order in dept_manager table

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,3 +1,6 @@
+-- Create the schema for the Pewlett Hackard Database
+
+-- Create tables, primary keys, and foreign keys
 CREATE TABLE IF NOT EXISTS titles
 (
     title_id VARCHAR(5) NOT NULL,
@@ -5,12 +8,11 @@ CREATE TABLE IF NOT EXISTS titles
     PRIMARY KEY(title_id)
 );
 
-CREATE TABLE IF NOT EXISTS salaries
+CREATE TABLE IF NOT EXISTS departments
 (
-    emp_no INTEGER NOT NULL,
-    salary DECIMAL(10, 2),
-    employees_emp_no INTEGER UNIQUE,
-    PRIMARY KEY(emp_no)
+    dept_no VARCHAR(4) NOT NULL,
+    dept_name VARCHAR(20),
+    PRIMARY KEY(dept_no)
 );
 
 CREATE TABLE IF NOT EXISTS employees
@@ -22,101 +24,32 @@ CREATE TABLE IF NOT EXISTS employees
     last_name VARCHAR(20),
     sex VARCHAR(1),
     hire_date DATE,
-    PRIMARY KEY(emp_no)
+    PRIMARY KEY(emp_no),
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE IF NOT EXISTS salaries
+(
+    emp_no INTEGER NOT NULL UNIQUE,
+    salary DECIMAL(10, 2),
+    PRIMARY KEY(emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE IF NOT EXISTS dept_manager
 (
-    emp_no INTEGER NOT NULL,
-    dept_no VARCHAR(4),
-    employees_emp_no INTEGER UNIQUE,
-    PRIMARY KEY(emp_no)
+    dept_no VARCHAR(4) NOT NULL,
+    emp_no INTEGER NOT NULL UNIQUE,
+    PRIMARY KEY(emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE IF NOT EXISTS dept_emp
 (
-    employees_emp_no INTEGER UNIQUE,
     emp_no INTEGER NOT NULL,
-    dept_no VARCHAR(4),
-    PRIMARY KEY(emp_no)
-);
-
-CREATE TABLE IF NOT EXISTS departments
-(
     dept_no VARCHAR(4) NOT NULL,
-    dept_name VARCHAR(20),
-    PRIMARY KEY(dept_no)
+    PRIMARY KEY(emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
-
--- Create Foreign Keys
-ALTER TABLE employees
-    ADD FOREIGN KEY (emp_no)
-    REFERENCES dept_emp(emp_no)
-    MATCH SIMPLE
-;
-
-ALTER TABLE employees
-    ADD FOREIGN KEY (emp_no)
-    REFERENCES dept_manager(emp_no)
-    MATCH SIMPLE
-;
-
-ALTER TABLE dept_emp
-    ADD FOREIGN KEY (emp_no)
-    REFERENCES employees(emp_no)
-    MATCH SIMPLE
-;
-    	
-ALTER TABLE employees
-    ADD FOREIGN KEY (emp_no)
-    REFERENCES salaries(emp_no)
-    MATCH SIMPLE
-;
-    
-ALTER TABLE employees
-    ADD CONSTRAINT FK_titles
-    FOREIGN KEY (emp_title_id)
-    REFERENCES titles(title_id)
-    MATCH SIMPLE
-;
-    
-ALTER TABLE dept_emp
-    ADD CONSTRAINT FK_departments
-    FOREIGN KEY (dept_no)
-    REFERENCES departments(dept_no)
-    MATCH SIMPLE
-;
-    
-ALTER TABLE dept_manager
-    ADD CONSTRAINT FK_departments
-    FOREIGN KEY (dept_no)
-    REFERENCES departments(dept_no)
-    MATCH SIMPLE
-;
-
-ALTER TABLE salaries
-    ADD FOREIGN KEY (emp_no)
-    REFERENCES employees(emp_no)
-    MATCH SIMPLE
-;
-    
-ALTER TABLE salaries
-    ADD CONSTRAINT FK_employees
-    FOREIGN KEY (employees_emp_no)
-    REFERENCES employees(emp_no)
-    MATCH SIMPLE
-;
-    
-ALTER TABLE dept_emp
-    ADD CONSTRAINT FK_employees
-    FOREIGN KEY (employees_emp_no)
-    REFERENCES employees(emp_no)
-    MATCH SIMPLE
-;
-    
-ALTER TABLE dept_manager
-    ADD CONSTRAINT FK_employees
-    FOREIGN KEY (employees_emp_no)
-    REFERENCES employees(emp_no)
-    MATCH SIMPLE
-;
