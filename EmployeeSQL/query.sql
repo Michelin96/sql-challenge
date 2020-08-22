520bff5c622496a75a1f4609e3bbb2f330f73591
MichelleH <voirdire96@gmail.com>
2020-08-22 16:48:18 -0700

    Completed queries checked with revised schema

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -4,8 +4,8 @@
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
 --Add the salary data to the info from the employee table
-INNER JOIN salaries s ON
-e.emp_no=s.emp_no;
+INNER JOIN salaries s
+ON e.emp_no=s.emp_no;
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
 -- Thanks to javapoint.com for the tip on selecting from the date data type.
@@ -17,14 +17,14 @@ WHERE hire_date <= '1986-12-31' AND hire_date >= '1986-01-01';
 -- List the manager of each department with the following information: 
 -- department number, department name, the manager's employee number, last name, first name.
 
-SELECT d.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+SELECT d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
 FROM employees e
 --Add the department managers to the info from the employee table
-INNER JOIN dept_manager dm ON
-e.emp_no=dm.emp_no
+INNER JOIN dept_manager dm 
+ON e.emp_no=dm.emp_no
 --Add the departments to the info from the employee table
-INNER JOIN departments d ON 
-d.dept_no=dm.dept_no;
+INNER JOIN departments d 
+ON d.dept_no=dm.dept_no;
 
 -- List the department of each employee with the following information:
 -- employee number, last name, first name, and department name.
@@ -32,11 +32,11 @@ d.dept_no=dm.dept_no;
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
 --Add the department employees to the info from the employee table
-INNER JOIN dept_emp de ON
-e.emp_no=de.emp_no
+INNER JOIN dept_emp de 
+ON e.emp_no=de.emp_no
 --Add the departments to the info from the employee table
-INNER JOIN departments d ON
-d.dept_no=de.dept_no;
+INNER JOIN departments d
+ON d.dept_no=de.dept_no;
 
 -- List first name, last name, and sex for employees 
 -- whose first name is "Hercules" and last names begin with "B."
