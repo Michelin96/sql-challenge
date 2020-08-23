a6503f37c5ed25034163ba208f196827f81ed0e2
MichelleH <voirdire96@gmail.com>
2020-08-22 17:10:16 -0700

    Increased data type limits

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -3,25 +3,25 @@
 -- Create tables, primary keys, and foreign keys
 CREATE TABLE IF NOT EXISTS titles
 (
-    title_id VARCHAR(5) NOT NULL,
-    title VARCHAR(20),
+    title_id VARCHAR(10) NOT NULL,
+    title VARCHAR(30),
     PRIMARY KEY(title_id)
 );
 
 CREATE TABLE IF NOT EXISTS departments
 (
-    dept_no VARCHAR(4) NOT NULL,
-    dept_name VARCHAR(20),
+    dept_no VARCHAR(10) NOT NULL,
+    dept_name VARCHAR(30),
     PRIMARY KEY(dept_no)
 );
 
 CREATE TABLE IF NOT EXISTS employees
 (
     emp_no INTEGER NOT NULL UNIQUE,
-    emp_title_id VARCHAR(5),
+    emp_title_id VARCHAR(10),
     birth_date DATE,
-    first_name VARCHAR(20),
-    last_name VARCHAR(20),
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     sex VARCHAR(1),
     hire_date DATE,
     PRIMARY KEY(emp_no),
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS salaries
 
 CREATE TABLE IF NOT EXISTS dept_manager
 (
-    dept_no VARCHAR(4) NOT NULL,
+    dept_no VARCHAR(10) NOT NULL,
     emp_no INTEGER NOT NULL UNIQUE,
     PRIMARY KEY(emp_no, dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS dept_manager
 CREATE TABLE IF NOT EXISTS dept_emp
 (
     emp_no INTEGER NOT NULL,
-    dept_no VARCHAR(4) NOT NULL,
+    dept_no VARCHAR(10) NOT NULL,
     PRIMARY KEY(emp_no, dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
